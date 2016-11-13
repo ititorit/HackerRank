d32fd63d8a90a2f412e027435be323032ef2b4c6
Nguyễn Minh Tuấn <minhtuancntt93@gmail.com>
2016-11-13 14:44:22 +0700

    Create angry-professor.cpp

---
+++ angry-professor.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _for(i,a,b) for(int i = a; i <= b; i++)
+#define _ford(i,a,b) for(int i = a; i >= b; i--)
+#define _it(i,v) for (typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
+#define _all(v) v.begin(), v.end()
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+ 
+#define DEBUG(x) { cout << "--> " << #x << " = " << x << '\n'; }
+#define sqr(x) ((x)*(x))
+#define reset(a,val) memset(a, val, sizeof(a))
+
+#define INF         1011111111
+#define EPS         (double)1e-8
+#define mod         1000000007
+#define PI          3.14159265358979
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int test, n, k, cnt, temp;
+    cin >> test;
+    while(test--) {
+        cnt = 0;
+        cin >> n >> k;
+        _for(i,1,n) {
+            cin >> temp;
+            if(temp <= 0) cnt++;
+        }
+        if(cnt >= k) {
+            cout << "NO\n";
+        } else cout << "YES\n";
+    }
+    return 0;
+}
